5d6b81befe5732222404ea121893afaa3f1aef54
Bruno Barros <brun.barros1@gmail.com>
2022-04-06 19:53:29 -0300

    Update MovieLocalDatasourceImpl

--- app/src/main/java/com/app/tmdbclient/data/repository/MovieLocalDatasourceImpl.kt
+++ app/src/main/java/com/app/tmdbclient/data/repository/MovieLocalDatasourceImpl.kt
@@ -2,6 +2,9 @@ package com.app.tmdbclient.data.repository
 
 import com.app.tmdbclient.data.db.MovieDAO
 import com.app.tmdbclient.data.model.movie.Movie
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MovieLocalDatasourceImpl(private val movieDAO: MovieDAO) : MovieLocalDatasource {
     override suspend fun getMoviesFromDB(): List<Movie> {
@@ -9,10 +12,14 @@ class MovieLocalDatasourceImpl(private val movieDAO: MovieDAO) : MovieLocalDatas
     }
 
     override suspend fun saveMoviesToDB(movies: List<Movie>) {
-        TODO("Not yet implemented")
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDAO.saveMovies(movies)
+        }
     }
 
     override suspend fun clearAll() {
-        TODO("Not yet implemented")
+        CoroutineScope(Dispatchers.IO).launch {
+            movieDAO.deleteAllMovies()
+        }
     }
 }
