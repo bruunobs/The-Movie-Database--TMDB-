9b416f99e9c3b8c9f69435ed26c5d63de6bffc9d
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 20:51:34 -0300

    Update HomeActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.app.tmdbclient.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
+import com.app.tmdbclient.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
+    
     }
 }
