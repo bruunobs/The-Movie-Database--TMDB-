9093dc70a92730887a0073fec83c766f7af60d26
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 20:53:29 -0300

    Create Movie Activity

--- app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
@@ -12,6 +12,6 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
-    
+
     }
 }
---
+++ app/src/main/java/com/app/tmdbclient/presentation/movie/MovieActivity.kt
@@ -0,0 +1,12 @@
+package com.app.tmdbclient.presentation.movie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.tmdbclient.R
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
