59826e9ce579e25c700cf2d4057f357e446b1991
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 20:57:06 -0300

    Update TvShowActivity

--- app/src/main/java/com/app/tmdbclient/presentation/tv/TvShowActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/tv/TvShowActivity.kt
@@ -2,11 +2,15 @@ package com.app.tmdbclient.presentation.tv
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
+import com.app.tmdbclient.databinding.ActivityTvShowBinding
 
 class TvShowActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTvShowBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tv_show)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_tv_show)
     }
 }
