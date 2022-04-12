76809e3f4be48159aaa62d3e620f50d491f7a743
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 20:54:10 -0300

    Update MovieActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/movie/MovieActivity.kt
@@ -2,11 +2,15 @@ package com.app.tmdbclient.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
+import com.app.tmdbclient.databinding.ActivityMovieBinding
 
 class MovieActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMovieBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_movie)
     }
 }
