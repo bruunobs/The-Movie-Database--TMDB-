eff7092cdfe1dfe2f47732b1cff8d6d7b7456bf9
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 20:55:21 -0300

    Create TvShow Activity

---
+++ app/src/main/java/com/app/tmdbclient/presentation/tv/TvShowActivity.kt
@@ -0,0 +1,12 @@
+package com.app.tmdbclient.presentation.tv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.tmdbclient.R
+
+class TvShowActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv_show)
+    }
+}
