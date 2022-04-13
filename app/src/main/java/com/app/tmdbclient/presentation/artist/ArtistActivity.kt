cb064c1839676344f877ee568fe03174cbbbfbde
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 21:00:36 -0300

    Update ArtistActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistActivity.kt
@@ -2,11 +2,15 @@ package com.app.tmdbclient.presentation.artist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
+import com.app.tmdbclient.databinding.ActivityArtistBinding
 
 class ArtistActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityArtistBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_artist)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_artist)
     }
 }
