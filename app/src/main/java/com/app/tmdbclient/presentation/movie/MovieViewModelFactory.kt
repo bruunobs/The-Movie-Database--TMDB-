5e37723bd8289e5604a52985d4615ba37de3ded2
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 15:54:25 -0300

    Create MovieViewModelFactory.kt

---
+++ app/src/main/java/com/app/tmdbclient/presentation/movie/MovieViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.app.tmdbclient.presentation.movie
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.app.tmdbclient.domain.usecase.GetMoviesUseCase
+import com.app.tmdbclient.domain.usecase.UpdateMoviesUseCase
+
+class MovieViewModelFactory(
+    private val getMoviesUseCase: GetMoviesUseCase,
+    private val updateMoviesUseCase: UpdateMoviesUseCase)
+    : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MovieViewModel(getMoviesUseCase,updateMoviesUseCase) as T
+    }
+
+
+}
