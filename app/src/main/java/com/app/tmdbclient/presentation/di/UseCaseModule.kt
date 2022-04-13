885d585a6393089364058539956dd809665bde7b
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 17:09:35 -0300

    Update UseCaseModule.kt

--- app/src/main/java/com/app/tmdbclient/presentation/di/UseCaseModule.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/di/UseCaseModule.kt
@@ -1,7 +1,9 @@
 package com.app.tmdbclient.presentation.di
 
+import com.app.tmdbclient.domain.repository.ArtistRepository
 import com.app.tmdbclient.domain.repository.MovieRepository
-import com.app.tmdbclient.domain.usecase.GetMoviesUseCase
+import com.app.tmdbclient.domain.repository.TvShowRepository
+import com.app.tmdbclient.domain.usecase.*
 import dagger.Module
 import dagger.Provides
 
@@ -9,6 +11,28 @@ import dagger.Provides
 class UseCaseModule {
     @Provides
     fun provideGetMovieUseCase(movieRepository: MovieRepository) : GetMoviesUseCase{
-        return GetMoviesUseCase((movieRepository))
+        return GetMoviesUseCase(movieRepository)
+    }
+    @Provides
+    fun provideUpdateMovieUseCase(movieRepository: MovieRepository) : UpdateMoviesUseCase{
+        return UpdateMoviesUseCase(movieRepository)
+    }
+
+    @Provides
+    fun provideGetTvShowUseCase(tvShowRepository: TvShowRepository) : GetTvShowUseCase{
+        return GetTvShowUseCase(tvShowRepository)
+    }
+    @Provides
+    fun provideUpdateTvShowUseCase(tvShowRepository: TvShowRepository) : UpdateTvShowsUseCase{
+        return provideUpdateTvShowUseCase(tvShowRepository)
+    }
+
+    @Provides
+    fun provideGetArtistUseCase(artistRepository: ArtistRepository) : GetArtistUseCase{
+        return GetArtistUseCase(artistRepository)
+    }
+    @Provides
+    fun provideUpdateArtistUseCase(artistRepository: ArtistRepository) : UpdateArtistsUseCase{
+        return UpdateArtistsUseCase(artistRepository)
     }
 }
