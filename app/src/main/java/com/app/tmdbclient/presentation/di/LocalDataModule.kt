cf149ac125d8e26e893ef24655a5b9d764ff9f35
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 16:43:48 -0300

    Update LocalDataModule.kt

--- app/src/main/java/com/app/tmdbclient/presentation/di/LocalDataModule.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/di/LocalDataModule.kt
@@ -1,8 +1,14 @@
 package com.app.tmdbclient.presentation.di
 
+import com.app.tmdbclient.data.db.ArtistDAO
 import com.app.tmdbclient.data.db.MovieDAO
+import com.app.tmdbclient.data.db.TvShowDAO
+import com.app.tmdbclient.data.repository.artist.datasource.ArtistLocalDatasource
+import com.app.tmdbclient.data.repository.artist.datasourceImpl.ArtistLocalDatasourceImpl
 import com.app.tmdbclient.data.repository.movie.datasource.MovieLocalDatasource
 import com.app.tmdbclient.data.repository.movie.datasourceImpl.MovieLocalDatasourceImpl
+import com.app.tmdbclient.data.repository.tvshow.datasource.TvShowLocalDatasource
+import com.app.tmdbclient.data.repository.tvshow.datasourceImpl.TvShowLocalDatasourceImpl
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -13,5 +19,17 @@ class LocalDataModule {
     @Provides
     fun provideMovieLocalDataSource(movieDAO: MovieDAO) : MovieLocalDatasource{
         return MovieLocalDatasourceImpl(movieDAO)
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowLocalDataSource(tvShowDAO: TvShowDAO) : TvShowLocalDatasource{
+        return TvShowLocalDatasourceImpl(tvShowDAO)
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistLocalDataSource(artistDAO: ArtistDAO) : ArtistLocalDatasource{
+        return ArtistLocalDatasourceImpl(artistDAO)
     }
 }
