6615d135782168fd1e078402f15e20ca8f4c1ac4
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 17:15:27 -0300

    Create AppComponent.kt

---
+++ app/src/main/java/com/app/tmdbclient/presentation/di/AppComponent.kt
@@ -0,0 +1,18 @@
+package com.app.tmdbclient.presentation.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [
+    AppModule::class,
+    NetModule::class,
+    DataBaseModule::class,
+    UseCaseModule::class,
+    RepositoryModule::class,
+    RemoteDataModule::class,
+    LocalDataModule::class,
+    CacheDataModule::class
+])
+interface AppComponent {
+}
