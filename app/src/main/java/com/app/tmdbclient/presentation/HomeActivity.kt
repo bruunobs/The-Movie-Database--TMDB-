6e02d4f1eb4f417f07ed3e6639c2afacf807243c
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 21:04:06 -0300

    Update HomeActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
 import com.app.tmdbclient.databinding.ActivityHomeBinding
+import com.app.tmdbclient.presentation.artist.ArtistActivity
 import com.app.tmdbclient.presentation.movie.MovieActivity
+import com.app.tmdbclient.presentation.tv.TvShowActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -16,6 +18,14 @@ class HomeActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
         binding.movieButton.setOnClickListener {
             val intent = Intent(this,MovieActivity::class.java)
+            startActivity(intent)
+        }
+        binding.artistsButton.setOnClickListener {
+            val intent = Intent(this,ArtistActivity::class.java)
+            startActivity(intent)
+        }
+        binding.tvButton.setOnClickListener {
+            val intent = Intent(this,TvShowActivity::class.java)
             startActivity(intent)
         }
     }
