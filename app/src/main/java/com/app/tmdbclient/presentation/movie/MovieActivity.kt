15a6cb101d91d8a284be58ae06a1401f60c125b4
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 18:09:51 -0300

    Update MovieActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/movie/MovieActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/movie/MovieActivity.kt
@@ -2,15 +2,32 @@ package com.app.tmdbclient.presentation.movie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.app.tmdbclient.R
 import com.app.tmdbclient.databinding.ActivityMovieBinding
+import com.app.tmdbclient.presentation.di.Injector
+import javax.inject.Inject
 
 class MovieActivity : AppCompatActivity() {
+    @Inject
+    lateinit var factory: MovieViewModelFactory
+    private lateinit var movieViewModel: MovieViewModel
     private lateinit var binding: ActivityMovieBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_movie)
+        (application as Injector).createMovieSubComponent()
+            .inject(this)
+
+        movieViewModel = ViewModelProvider(this,factory)
+            .get(MovieViewModel::class.java)
+        val responseLiveData = movieViewModel.getMovies()
+        responseLiveData.observe(this, Observer {
+            Log.i("TAG",it.toString())
+        })
     }
 }
