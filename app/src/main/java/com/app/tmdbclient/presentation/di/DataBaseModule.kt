d4593c83e44a9f26088bc25dd8b8a953478ea859
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 16:35:51 -0300

    Update DataBaseModule.kt

--- app/src/main/java/com/app/tmdbclient/presentation/di/DataBaseModule.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/di/DataBaseModule.kt
@@ -2,7 +2,10 @@ package com.app.tmdbclient.presentation.di
 
 import android.content.Context
 import androidx.room.Room
+import com.app.tmdbclient.data.db.ArtistDAO
+import com.app.tmdbclient.data.db.MovieDAO
 import com.app.tmdbclient.data.db.TMDBDatabase
+import com.app.tmdbclient.data.db.TvShowDAO
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
@@ -14,5 +17,22 @@ class DataBaseModule {
     fun provideMovieDataBase(context: Context) : TMDBDatabase{
         return Room.databaseBuilder(context,TMDBDatabase::class.java,"tmdbclient")
             .build()
+    }
+    @Singleton
+    @Provides
+    fun provideMovieDao(tmdbDatabase: TMDBDatabase) : MovieDAO{
+        return tmdbDatabase.movieDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowDao(tmdbDatabase: TMDBDatabase) : TvShowDAO{
+        return tmdbDatabase.tvShowDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistDao(tmdbDatabase: TMDBDatabase) : ArtistDAO{
+        return tmdbDatabase.artistDao()
     }
 }
