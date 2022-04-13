742e83e3912ee2a6c023455d6285e3d05d012582
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 17:18:55 -0300

    Create ArtistModule.kt

---
+++ app/src/main/java/com/app/tmdbclient/presentation/di/artist/ArtistModule.kt
@@ -0,0 +1,20 @@
+package com.app.tmdbclient.presentation.di.artist
+
+import com.app.tmdbclient.domain.usecase.GetArtistUseCase
+import com.app.tmdbclient.domain.usecase.UpdateArtistsUseCase
+import com.app.tmdbclient.presentation.artist.ArtistViewModelFactory
+import dagger.Module
+import dagger.Provides
+
+@Module
+class ArtistModule {
+    @Provides
+    fun provideArtistViewModelFactory(
+        getArtistUseCase: GetArtistUseCase,
+        updateArtistsUseCase: UpdateArtistsUseCase
+    ) : ArtistViewModelFactory{
+        return ArtistViewModelFactory(
+            getArtistUseCase,updateArtistsUseCase
+        )
+    }
+}
