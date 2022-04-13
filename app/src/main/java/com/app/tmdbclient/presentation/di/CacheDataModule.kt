56df7fd1044ec835d3f7aef85bbe31e471c60a04
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 16:46:18 -0300

    Create CacheDataModule.kt

---
+++ app/src/main/java/com/app/tmdbclient/presentation/di/CacheDataModule.kt
@@ -0,0 +1,32 @@
+package com.app.tmdbclient.presentation.di
+
+import com.app.tmdbclient.data.repository.artist.datasource.ArtistCacheDatasource
+import com.app.tmdbclient.data.repository.artist.datasourceImpl.ArtistCacheDataSourceImpl
+import com.app.tmdbclient.data.repository.movie.datasource.MovieCacheDatasource
+import com.app.tmdbclient.data.repository.movie.datasourceImpl.MovieCacheDataSourceImpl
+import com.app.tmdbclient.data.repository.tvshow.datasource.TvShowCacheDatasource
+import com.app.tmdbclient.data.repository.tvshow.datasourceImpl.TvShowCacheDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class CacheDataModule {
+    @Singleton
+    @Provides
+    fun provideMovieCacheDataSource() : MovieCacheDatasource{
+        return MovieCacheDataSourceImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTvShowCacheDataSource() : TvShowCacheDatasource{
+        return TvShowCacheDataSourceImpl()
+    }
+
+    @Singleton
+    @Provides
+    fun provideArtistCacheDataSource() : ArtistCacheDatasource{
+        return ArtistCacheDataSourceImpl()
+    }
+}
