c3b7485624f9d54e57b3e39c4841f38da7bc43b0
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 17:29:52 -0300

    Update AppModule.kt

--- app/src/main/java/com/app/tmdbclient/presentation/di/core/AppModule.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/di/core/AppModule.kt
@@ -1,11 +1,14 @@
 package com.app.tmdbclient.presentation.di.core
 
 import android.content.Context
+import com.app.tmdbclient.presentation.di.artist.ArtistSubComponent
+import com.app.tmdbclient.presentation.di.movie.MovieSubComponent
+import com.app.tmdbclient.presentation.di.tv.TvShowSubComponent
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
-@Module
+@Module(subcomponents = [MovieSubComponent::class,ArtistSubComponent::class,TvShowSubComponent::class])
 class AppModule(private val context: Context) {
     @Singleton
     @Provides
