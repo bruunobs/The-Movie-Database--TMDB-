4def44ab1938fed36f3eb6043aea8242de3af9e8
Bruno Barros <brun.barros1@gmail.com>
2022-04-12 21:03:00 -0300

    Update HomeActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.app.tmdbclient.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.app.tmdbclient.R
 import com.app.tmdbclient.databinding.ActivityHomeBinding
+import com.app.tmdbclient.presentation.movie.MovieActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -12,6 +14,9 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
-
+        binding.movieButton.setOnClickListener {
+            val intent = Intent(this,MovieActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
