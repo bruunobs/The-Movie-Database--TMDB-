366001c54f5548885c5083e172fa1de37e02d97b
Bruno Barros <brun.barros1@gmail.com>
2022-04-13 16:11:01 -0300

    Create NetModule.kt

---
+++ app/src/main/java/com/app/tmdbclient/presentation/di/NetModule.kt
@@ -0,0 +1,20 @@
+package com.app.tmdbclient.presentation.di
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetModule(private val baseUrl : String) {
+    @Singleton
+    @Provides
+    fun provideRetrofit() : Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+
+    }
+}
