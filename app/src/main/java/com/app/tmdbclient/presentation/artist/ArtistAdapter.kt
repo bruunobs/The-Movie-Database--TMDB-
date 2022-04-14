8996d147215089b3eedb8b7ec994979312157143
Bruno Barros <brun.barros1@gmail.com>
2022-04-14 16:43:21 -0300

    Update TvShowActivity

--- app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistAdapter.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistAdapter.kt
@@ -1,16 +1,53 @@
 package com.app.tmdbclient.presentation.artist
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.app.tmdbclient.R
 import com.app.tmdbclient.data.model.artist.Artist
+import com.app.tmdbclient.data.model.tvshow.TvShow
 import com.app.tmdbclient.databinding.ListItemBinding
+import com.app.tmdbclient.presentation.movie.MyViewHolderTvShow
+import com.bumptech.glide.Glide
 
-class ArtistAdapter {
+class ArtistAdapter() : RecyclerView.Adapter<MyViewHolder>() {
+
+    private val artistList = ArrayList<Artist>()
+
+    fun setList(artist : List<Artist>){
+        artistList.clear()
+        artistList.addAll(artist)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding : ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(artistList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return artistList.size
+    }
 }
 
 class MyViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root){
 
     fun bind(artist : Artist){
         binding.titleTextView.text = artist.name
-
+        binding.descriptionTextView.text = artist.
+        val posterURL = "https://image.tmdb.org/t/p/w500"+artist.profilePath
+        Glide.with(binding.imageView.context)
+            .load(posterURL)
+            .into(binding.imageView)
     }
 }
