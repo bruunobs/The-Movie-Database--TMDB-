1d1fff8e5ca36d617da81d479462c86e342be5de
Bruno Barros <brun.barros1@gmail.com>
2022-04-14 16:48:00 -0300

    Update ArtistActivity.kt

--- app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistActivity.kt
+++ app/src/main/java/com/app/tmdbclient/presentation/artist/ArtistActivity.kt
@@ -2,15 +2,98 @@ package com.app.tmdbclient.presentation.artist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.app.tmdbclient.R
 import com.app.tmdbclient.databinding.ActivityArtistBinding
+import com.app.tmdbclient.databinding.ActivityTvShowBinding
+import com.app.tmdbclient.presentation.di.Injector
+import com.app.tmdbclient.presentation.movie.MovieAdapter
+import com.app.tmdbclient.presentation.movie.MovieViewModel
+import com.app.tmdbclient.presentation.movie.TvShowAdapter
+import com.app.tmdbclient.presentation.tv.TvShowViewModel
+import com.app.tmdbclient.presentation.tv.TvShowViewModelFactory
+import javax.inject.Inject
 
 class ArtistActivity : AppCompatActivity() {
+
+
+    @Inject
+    lateinit var factory: ArtistViewModelFactory
+    private lateinit var artistViewModel: ArtistViewModel
+    private lateinit var adapter: ArtistAdapter
     private lateinit var binding: ActivityArtistBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_artist)
+
+        (application as Injector).createArtistSubComponent()
+            .inject(this)
+
+        artistViewModel = ViewModelProvider(this,factory)
+            .get(ArtistViewModel::class.java)
+        initRecyclerView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateArtists()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateArtists(){
+        binding.artistProgressBar.visibility = View.VISIBLE
+        val response = artistViewModel.updateArtists()
+        response.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.artistProgressBar.visibility = View.GONE
+
+            }else{
+                binding.artistProgressBar.visibility = View.GONE
+
+            }
+        })
+    }
+
+    private fun initRecyclerView(){
+        binding.artistRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = ArtistAdapter()
+        binding.artistRecyclerView.adapter = adapter
+        displayPopularArtists()
+    }
+
+    private fun displayPopularArtists(){
+        binding.artistProgressBar.visibility = View.VISIBLE
+        val responseLiveData = artistViewModel.getArtists()
+        responseLiveData.observe(this, Observer {
+            if(it!=null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.artistProgressBar.visibility = View.GONE
+            }else{
+                binding.artistProgressBar.visibility = View.GONE
+                Toast.makeText(applicationContext,"No data available", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
